63e0e3f848a8721d44d1b51a279690b08956efee
Jimin <thswlals0219@gmail.com>
2025-05-06 19:49:36 +0900

    style: css 수정

--- dev-docs/src/components/HomepageFeatures/index.js
+++ dev-docs/src/components/HomepageFeatures/index.js
@@ -47,12 +47,12 @@ const FeatureList = [
 function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3" className={styles.title}>{title}</Heading>
-        <p className={styles.description}>{description}</p>
-      </div>
-      <div className="text--center">
+      <div className="text--center" style={{ marginBottom: '2rem' }}>
         <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
       </div>
     </div>
   );
